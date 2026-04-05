228f1e819f199c3b08a13f1861082da74c5ba28c
YonAnn99 <jonattan10.99@hotmail.com>
2026-04-05 16:47:49 -0600

    Ajuste de puerto y bind en Procfile 3.2

--- core/settings.py
+++ core/settings.py
@@ -132,4 +132,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
