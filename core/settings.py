0f951b936ebe2dfd6d7c8d504b139eefa5da4c80
YonAnn99 <jonattan10.99@hotmail.com>
2026-07-07 02:43:58 -0600

    Feat: Agregar IVA y Descuento al POS 2.0

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)ri21c!ubq8y!%fo_z%#o#yx9t60pt4&yyc-kos!txlubpx!+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'acrylitec.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://acrylitec.up.railway.app']
